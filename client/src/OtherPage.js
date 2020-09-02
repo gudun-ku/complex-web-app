93c8aee9293dbff2e903443786342d04c62a4b8a
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-09-02 15:33:15 +0400

    Light changes to clear readiness the page

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
     <div>
+      <div>
+        <br />
+      </div>
       Im some other page!
       <Link to="/">Go back home</Link>
     </div>
